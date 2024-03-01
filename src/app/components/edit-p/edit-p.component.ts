728d02fcf04ae0a985918fc1d3317c665258a238
72xarth <pikachu0940188505@gmail.com>
2024-03-01 22:49:25 +0700

    last

--- src/app/components/edit-p/edit-p.component.ts
+++ src/app/components/edit-p/edit-p.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { RouterLink,RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit-p',
   standalone: true,
-  imports: [MatInputModule,MatButtonModule,MatFormFieldModule],
+  imports: [MatInputModule,MatButtonModule,MatFormFieldModule,RouterModule,RouterLink],
   templateUrl: './edit-p.component.html',
   styleUrl: './edit-p.component.scss'
 })
--- src/app/components/login1/login1.component.ts
+++ src/app/components/login1/login1.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink, RouterModule } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-login1',
   standalone: true,
-  imports: [RouterModule,MatButtonModule,RouterModule],
+  imports: [RouterModule,MatButtonModule,RouterModule,RouterLink],
   templateUrl: './login1.component.html',
   styleUrl: './login1.component.scss'
 })
