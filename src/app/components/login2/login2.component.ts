925cab8a57e0150a08e3fb24aa0dac2f2b45b028
72xarth <pikachu0940188505@gmail.com>
2024-03-04 00:17:14 +0700

    Login success

--- src/app/components/login2/login2.component.ts
+++ src/app/components/login2/login2.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../../service/appdata.service';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login2',
   standalone: true,
-  imports: [RouterModule,MatButtonModule,MatIconModule,RouterLink],
+  imports: [RouterModule,MatButtonModule,MatIconModule,RouterLink,ReactiveFormsModule],
   templateUrl: './login2.component.html',
   styleUrl: './login2.component.scss'
 })
-export class Login2Component {
+export class Login2Component implements OnInit {
+  myForm!: FormGroup;
+  bool: any;
 
+  constructor(private formBuilder: FormBuilder, private api: ApiService,private route:Router) {}
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      gmail: [''],
+      password: [''],
+    });
+  }
+
+  async submitForm() {
+     let formData = this.myForm.value;
+    console.log(formData);
+    
+     this.Checklogin(formData);
+  
+  }
+
+  async Checklogin(jsonData: any) {
+    this.bool = await this.api.checkLogin(jsonData);
+ 
+  
+    
+    
+    
+    if(typeof this.bool.uid === 'number'){
+  
+        this.route.navigate(['/rank/']);
+    }
+    else{
+      console.log("wrong");
+      
+    }
+  }
 }
---
+++ src/app/service/appdata.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Constants } from '../config/constants';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  
+  constructor(private constants: Constants, private http: HttpClient) {}
+
+  // Login 
+  public async checkLogin(options?: any) {    
+    const url = `${this.constants.API_ENDPOINT}/test`;
+    console.log(url);
+    
+    return lastValueFrom(this.http.post(url, options));
+  }
+
+  // Register 
+  public async register(option: any) {
+    const url = `${this.constants.API_ENDPOINT}/user`;
+    return lastValueFrom(this.http.post(url, option));
+  }
+
+  // Get image
+  public async getImage() {    
+    const url = `${this.constants.API_ENDPOINT}/image`;
+    return lastValueFrom(this.http.get(url));
+  }
+
+  public async getImageAll() {    
+    const url = `${this.constants.API_ENDPOINT}/image/all`;
+    return lastValueFrom(this.http.get(url));
+  }
+
+  // Update point 
+  public async putPoint(options?: any) {    
+    const url = `${this.constants.API_ENDPOINT}/eloprocess`;
+    return lastValueFrom(this.http.put(url, options));
+  }
+
+  // Update imageUser 
+  public async imageUser(options?: any) {    
+    const url = `${this.constants.API_ENDPOINT}/upload/image`;
+    return lastValueFrom(this.http.put(url, options));
+  }
+
+  // Select last UID
+  public async lastUID(options?: any) {    
+    const url = `${this.constants.API_ENDPOINT}/upload`;
+    return lastValueFrom(this.http.get(url, options));
+  }
+
+  // Delete img
+  public async deleteIMG(imageId: string) {    
+    const url = `${this.constants.API_ENDPOINT}/image/${imageId}`;
+    return lastValueFrom(this.http.delete(url));
+  }
+
+  // Insert picture
+  public insertPicture(option?: any){
+    const url = `${this.constants.API_ENDPOINT}/upload`;
+    return this.http.post(url, option);
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppdataService {
+  constructor() { }
+}
