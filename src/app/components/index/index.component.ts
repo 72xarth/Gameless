6f6e721aa9e7c55fdc83a81c9b7c64b439dc7720
Common01 <67251868+Common01@users.noreply.github.com>
2024-03-24 21:11:54 +0700

    GG

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -7,6 +7,8 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { lastValueFrom } from 'rxjs';
 import { ApiService } from '../../service/appdata.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ResultDialogComponent } from '../login1/ResultDialogComponent';
 
 
 @Component({
@@ -19,7 +21,7 @@ import { ApiService } from '../../service/appdata.service';
 export class IndexComponent implements OnInit{
   
 
-  constructor(private http : HttpClient,private api : ApiService,private route:Router){}
+  constructor(private http : HttpClient,private api : ApiService,private route:Router, private dialog: MatDialog){}
   data! : any;
   data1! : any;
   data2! : any;
@@ -46,21 +48,34 @@ export class IndexComponent implements OnInit{
   }
 
   async click1() {
+    console.log("asas");
     let json = {
       
       win : "A",
       gidA : this.data[0].gid,
       scoreA : this.data[0].score,
+      scoreUp1 : this.data[0].scoreUp,
       gidB : this.data[1].gid,
-      scoreB : this.data[1].score
+      scoreB : this.data[1].score,
+      scoreDown1 : this.data[1].scoreDown,
+      newScore1: this.data[0].newScore1,
+      newScore2: this.data[1].newScore2
 
     }
     this.findOne();
   
-    
-    let x = await this.api.updatescore(json);
-    console.log(x);
-    
+    try {
+      let x = await this.api.updatescore(json);
+  
+   
+
+      this.openDialog(x);
+    } catch (error) {
+      
+    }
+     
+   
+
 }
 
   async click2(){
@@ -69,16 +84,34 @@ export class IndexComponent implements OnInit{
       win : "B",
       gidA : this.data[0].gid,
       scoreA : this.data[0].score,
+      scoreUp1 : this.data[0].scoreUp,
       gidB : this.data[1].gid,
-      scoreB : this.data[1].score
+      scoreB : this.data[1].score,
+      scoreDown1 : this.data[1].scoreDown,
+      newScore1: this.data[0].newScore1,
+      newScore2: this.data[1].newScore2
 
     }
     this.findOne();
-    let x = await this.api.updatescore(json);
-    console.log(x);
+    
+    try {
+      let x = await this.api.updatescore(json);
+  
+   
 
+      this.openDialog(x);
+    } catch (error) {
+      
+    }
 
   }
+
+  openDialog(data: any): void {
+    const dialogRef = this.dialog.open(ResultDialogComponent, {
+      width: '250px',
+      data: data
+    });
+}
 
   logout(){
       localStorage.clear();
