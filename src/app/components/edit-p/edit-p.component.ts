168be2c2e9658143e08b2d64f3c45347cd4c4a06
72xarth <pikachu0940188505@gmail.com>
2024-03-24 22:25:22 +0700

    //////

--- src/app/components/edit-p/edit-p.component.ts
+++ src/app/components/edit-p/edit-p.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { RouterLink,RouterModule } from '@angular/router';
+import { Router, RouterLink,RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ApiService } from '../../service/appdata.service';
 
 @Component({
   selector: 'app-edit-p',
@@ -12,7 +14,34 @@ import { RouterLink,RouterModule } from '@angular/router';
   styleUrl: './edit-p.component.scss'
 })
 export class EditPComponent {
+
+  constructor(private formBuilder: FormBuilder, private api: ApiService,private route:Router) {}
+  id : any;
+  image : any;
+  email : any;
+  name : any;
+  myForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      Nname: [''],
+      // Npassword: [''],
+      // Nimage: [''],
+    });
+    
+    this.image = localStorage.getItem('url');
+    this.id = localStorage.getItem('id');
+    this.email = localStorage.getItem('gmail');
+    this.name = localStorage.getItem('name');
+  }
+
+submitForm() {
+  this.api.editPro(this.myForm);
+  console.log(this.myForm.value);
+  
+}
   back() {
     window.history.back();
 }
+
 }
